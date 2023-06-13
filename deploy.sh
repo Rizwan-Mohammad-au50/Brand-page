2e54c68c19e0cb8e4b5d2b6bf82cff494ce34a96
Rizwan-Mohammad-au50 <rizzu786687@gmail.com>
2023-06-13 22:02:48 +0530

    second commit

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ git init
 git checkout -B main
 git add -A
 
-git commit -m ‘deploy’
+git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push —-f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
